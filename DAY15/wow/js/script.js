7896021f499dd455bb97f38eb50939077b4f1665
tud9711 <tud9711@naver.com>
2021-12-28 21:53:38 +0900

    18일차

---
+++ DAY15/wow/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // 요소와 상단 사이 거리
+        mobile:       true,       // default
+        live:         true,       // default
+      }
+      )
+      wow.init();
+})
