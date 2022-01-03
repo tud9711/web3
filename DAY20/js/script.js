3ba15b9137ffe2ed23e6eb45fff552fb054cf8b0
tud9711 <tud9711@naver.com>
2022-01-03 21:56:35 +0900

    웹3종강

--- DAY20/js/script.js
+++ DAY20/js/script.js
@@ -24,6 +24,7 @@ $(function(){
 let scrollTop = 0
 // 스크롤 감지
 $(window).on('scroll resize', function() {
+    // 스크롤 위치
     scrollTop = $(this).scrollTop()
     if( scrollTop > 200 ) {
         $('header').addClass('on')
@@ -31,5 +32,55 @@ $(window).on('scroll resize', function() {
         $('header').removeClass('on')
     }
 
+    // 전체 높이
+    let containerHeight = $('.container').height()
+
+    // 윈도우 높이
+    let windowHeight = $(window).height()
+
+    // 인디케이터 퍼센트
+    let percent = 0
+
+    if ( scrollTop <= windowHeight ) {
+        percent = scrollTop / containerHeight * 100
+    } else {
+        percent = (scrollTop + windowHeight) / containerHeight * 100
+     }
+
+    $('.progress-bar').css({'width' : percent + "%" })
+
+
 })
 
+// 모바일 햄버거 버튼
+$(function(){
+    // 모바일 햄버거 버튼 클릭
+    $('header .open').on('click', function(){
+        $('header .bg').fadeIn()
+        $('header nav').addClass('on')
+        $('header nav').css({'width' : '300px'})
+    })
+
+    $('header .close, hedaer .bg').on('click', function(){
+        $('header .bg').fadeOut()
+        $('header nav').removeClass('on')
+        $('header nav').css({'width' : '0'})
+    })
+})
+
+
+// 플로팅 버튼 클릭
+let floatOn = 'off'
+$(function(){
+    $('.floating-list li').on('click', function(){
+        if(floatOn == 'off'){
+         $('#floating-btn1').css({ transform  : "translateY(-260px)", transition : "all 0.3s"})
+         $('#floating-btn2').css({ transform  : "translateY(-130px)", transition : "all 0.3s"})
+         floatOn = 'on'
+        } else {
+            $('#floating-btn1').css({ transform  : "translateY(260px)", transition : "all 0.3s"})
+            $('#floating-btn2').css({ transform  : "translateY(130px)", transition : "all 0.3s"})
+            floatOn = 'off'
+        }
+    })
+})
